31bf9e8d83af7e59e5156bf441d8abfbb6ed2dbc
alightedlamp <alightedlamp@gmail.com>
2017-10-29 09:58:41 -0500

    Add spacer, update letters

--- emojifyPhrase.js
+++ emojifyPhrase.js
@@ -1,15 +1,17 @@
-const letterMap = require('./letterMap');
+const letterMap = require("./letterMap");
 
 function emojifyPhrase(str, emoji) {
     // Constrain output to Slack's message limit
     // To do: figure out what that limit actually is
     const maxLength = 1000;
 
-    const spacer = ' '.repeat(6);
+    const spacer = ":spacer:";
 
-    const replaceChars = (layer) => layer.replace(/[s]/g, spacer).replace(/[x]/g, `:${emoji}:`);
-    let emojified = [...str].map(char => letterMap[char.toUpperCase()]
-                            .map(layer => replaceChars(layer), []));
+    const replaceChars = layer =>
+        layer.replace(/[s]/g, spacer).replace(/[x]/g, `:${emoji}:`);
+    let emojified = [...str].map(char =>
+        letterMap[char.toUpperCase()].map(layer => replaceChars(layer), [])
+    );
 
     for (let i = 1; i < emojified.length; i++) {
         for (let j = 0; j < emojified[i].length; j++) {
@@ -17,9 +19,9 @@ function emojifyPhrase(str, emoji) {
         }
     }
     return emojified.length < maxLength
-        ? emojified[0].join('\n')
+        ? emojified[0].join("\n")
         : `Sorry, message is ${emojified.length} characters, \
            but must be within ${maxLength} characters!`;
 }
 
-console.log(emojifyPhrase('art', 'art'));
+console.log(emojifyPhrase("yes!", "success"));
